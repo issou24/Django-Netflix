9b6a2763254b1aca4627c1a64920ae9aa52f2cbd
FIRST_NAME LAST_NAME <l.frz65@gmail.com>
2025-04-22 21:03:19 +0200

    com 2

--- core/views.py
+++ core/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 class Home(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
+
+class ProfileList(View):
+    def get(self, request, *args, **kwargs):
+        profiles=request.user.profiles.all()
+        return render(request, 'profileList.html',{
+            'profiles':profiles
+        })
