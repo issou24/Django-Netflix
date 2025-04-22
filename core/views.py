acab221394f65a770a1192dde5b7392961a079c6
FIRST_NAME LAST_NAME <l.frz65@gmail.com>
2025-04-22 21:19:51 +0200

    com profile

--- core/views.py
+++ core/views.py
@@ -7,6 +7,7 @@ class Home(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html')
 
+@method_decorator(login_required,name='dispatch')
 class ProfileList(View):
     def get(self, request, *args, **kwargs):
         profiles=request.user.profiles.all()
