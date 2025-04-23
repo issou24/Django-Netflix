3b97a077f5401e0eed1053d5a6e5165d2381915e
FIRST_NAME LAST_NAME <l.frz65@gmail.com>
2025-04-23 10:32:55 +0200

    prof debuged

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
-from .forms import Profile
+from .forms import ProfileForm
 
 class Home(View):
     def get(self,request,*args, **kwargs):
