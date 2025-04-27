8a994cf02d4c5d5164a30d6497f329e101edcfaf
FIRST_NAME LAST_NAME <l.frz65@gmail.com>
2025-04-28 00:28:16 +0200

    test

--- core/views.py
+++ core/views.py
@@ -1,25 +1,30 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-
 from .forms import ProfileForm
 from .models import Movie, Profile
 
 class Home(View):
     def get(self,request,*args, **kwargs):
         if request.user.is_authenticated:
-            return redirect('core:profile_list')
+            return redirect(to='/profile/')
         return render(request,'index.html')
 
 @method_decorator(login_required,name='dispatch')
 class ProfileList(View):
-    def get(self, request, *args, **kwargs):
+    
+    def get(self,request,*args, **kwargs):
+
         profiles=request.user.profiles.all()
+
         print(profiles)
-        return render(request, 'profileList.html',{
+
+
+        return render(request,'profileList.html',{
             'profiles':profiles
         })
+
 
 @method_decorator(login_required,name='dispatch')
 class ProfileCreate(View):
@@ -53,13 +58,21 @@ class Watch(View):
 
             movies=Movie.objects.filter(age_limit=profile.age_limit)
 
+            try:
+                showcase=movies[0]
+            except :
+                showcase=None
+            
+
             if profile not in request.user.profiles.all():
                 return redirect(to='core:profile_list')
             return render(request,'movieList.html',{
             'movies':movies,
+            'show_case':showcase
             })
         except Profile.DoesNotExist:
             return redirect(to='core:profile_list')
+
 
 @method_decorator(login_required,name='dispatch')
 class ShowMovieDetail(View):
@@ -73,7 +86,6 @@ class ShowMovieDetail(View):
             })
         except Movie.DoesNotExist:
             return redirect('core:profile_list')
-
 
 @method_decorator(login_required,name='dispatch')
 class ShowMovie(View):
